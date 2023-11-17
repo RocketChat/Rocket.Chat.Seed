554b541b59950e39d366eb6e5f74e4bf490e80fb
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2023-11-17 20:39:17 -0300

    Add caching of token to prevent multiple logins

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
+import fs from 'fs';
 import yargs from 'yargs';
 import axios from 'axios';
 import { faker } from '@faker-js/faker';
 import 'axios-debug-log';
 
 axios.defaults.family = 4;
+
+const filePath = 'auth_cache.json';
 
 class RestClient {
 	authToken?: string;
@@ -13,8 +16,28 @@ class RestClient {
 		axios.defaults.baseURL = this.host;
 	}
 
+	async isAuthValid() {
+		const endpoint = `/api/v1/me`;
+
+		try {
+			const response = await axios.get(endpoint);
+
+			if (response.data.success !== true) {
+				throw new Error(`Failed to get me: ${response.data}`);
+			}
+
+			return true;
+		} catch (error: any) {
+			return false;
+		}
+	}
+
 	async connect(username: string, password: string) {
 		const endpoint = `/api/v1/login`;
+
+		if (this.authToken && await this.isAuthValid()) {
+			return;
+		}
 
 		try {
 			const response = await axios.post(endpoint, { username, password });
@@ -43,9 +66,37 @@ class RestClient {
 			this.authToken = response.data.data.authToken;
 			this.userId = response.data.data.userId;
 
+			this.saveAuth();
 		} catch (error: any) {
 			throw new Error(`Failed to connect: ${error.message}`);
 		}
+	}
+
+	// this function saves the auth token and user id from the class into a file
+	// to reuse it later
+	async saveAuth() {
+		const data = {
+			authToken: this.authToken,
+			userId: this.userId,
+		};
+
+
+		fs.writeFileSync(filePath, JSON.stringify(data));
+	}
+
+	// this function loads the auth token and user id from a file
+	async loadAuth() {
+		if (!fs.existsSync(filePath)) {
+			return;
+		}
+
+		const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+		this.authToken = data.authToken;
+		this.userId = data.userId;
+
+		axios.defaults.headers.common['X-Auth-Token'] = this.authToken;
+		axios.defaults.headers.common['X-User-Id'] = this.userId;
 	}
 
 	async logout() {
@@ -134,6 +185,8 @@ async function main() {
 
 	const restClient = new RestClient(argv.host);
 
+	await restClient.loadAuth();
+
 	await restClient.connect(argv.user, argv.password);
 
 	// Call the appropriate function based on the command line arguments
@@ -141,7 +194,7 @@ async function main() {
 		await restClient.createUsers(argv.users);
 	}
 
-	await restClient.logout();
+	// await restClient.logout();
 }
 
 main().catch((error) => {
